5905315830ddd99bfcaeda699fd80fde549dc3eb
Steve Johns <steve@stevejay.net>
2022-01-06 22:36:03 +0000

    tidy up

--- src/pages/Histogram/index.tsx
+++ src/pages/Histogram/index.tsx
@@ -1,18 +1,9 @@
-import { HorizontalRule } from '@/components/HorizontalRule';
 import { PageHeading } from '@/components/PageHeading';
-
-// import { HorizontalBarChartExamples } from './HorizontalBarChartExamples';
-// import { VerticalBarChartExamples } from './VerticalBarChartExamples';
 
 const HistogramPage = () => (
   <>
     <PageHeading>Histogram</PageHeading>
-    <div className="w-full max-w-3xl p-4 mx-auto space-y-8 md:p-8">
-      {/* <VerticalBarChartExamples transitionSeconds={0.5} /> */}
-      TODO
-      <HorizontalRule />
-      {/* <HorizontalBarChartExamples transitionSeconds={0.5} /> */}
-    </div>
+    <div className="w-full max-w-3xl p-4 mx-auto space-y-8 md:p-8">TODO</div>
   </>
 );
 
