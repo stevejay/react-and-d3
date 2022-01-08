19234a9015f606792b74eca455c1b3292173b310
Steve Johns <steve@stevejay.net>
2022-01-08 21:26:51 +0000

    improve cards

--- src/pages/Home/illustrations/GroupedBarChartSvg.tsx
+++ src/pages/Home/illustrations/GroupedBarChartSvg.tsx
@@ -9,19 +9,15 @@ export const GroupedBarChartSvg = () => (
     fill="currentColor"
   >
     <rect x={8} y={64} width={16} height={80} />
-    <rect x={28} y={30} width={16} height={114} />
-    <rect x={48} y={44} width={16} height={100} />
+    <rect x={28} y={30} width={16} height={114} className="opacity-80" />
+    <rect x={48} y={44} width={16} height={100} className="opacity-60" />
 
-    <rect x={72} y={30} width={16} height={114} />
-    <rect x={92} y={0} width={16} height={144} />
-    <rect x={112} y={10} width={16} height={134} />
-
-    <rect x={136} y={130} width={16} height={14} />
-    <rect x={156} y={110} width={16} height={34} />
-    <rect x={176} y={139} width={16} height={5} />
+    <rect x={104} y={30} width={16} height={114} />
+    <rect x={124} y={0} width={16} height={144} className="opacity-80" />
+    <rect x={144} y={10} width={16} height={134} className="opacity-60" />
 
     <rect x={200} y={80} width={16} height={64} />
-    <rect x={220} y={90} width={16} height={54} />
-    <rect x={240} y={124} width={16} height={20} />
+    <rect x={220} y={90} width={16} height={54} className="opacity-80" />
+    <rect x={240} y={64} width={16} height={80} className="opacity-60" />
   </svg>
 );
--- src/pages/Home/illustrations/StackedBarChartSvg.tsx
+++ src/pages/Home/illustrations/StackedBarChartSvg.tsx
@@ -8,19 +8,19 @@ export const StackedBarChartSvg = () => (
     className="mx-auto border-b-2 border-l-2 text-slate-50 border-slate-50"
     fill="currentColor"
   >
-    <rect x={8} y={30} width={56} height={26} />
-    <rect x={8} y={60} width={56} height={16} />
+    <rect x={8} y={30} width={56} height={26} className="opacity-60" />
+    <rect x={8} y={60} width={56} height={16} className="opacity-80" />
     <rect x={8} y={80} width={56} height={64} />
 
-    <rect x={72} y={0} width={56} height={82} />
-    <rect x={72} y={86} width={56} height={30} />
+    <rect x={72} y={0} width={56} height={82} className="opacity-60" />
+    <rect x={72} y={86} width={56} height={30} className="opacity-80" />
     <rect x={72} y={120} width={56} height={24} />
 
-    <rect x={136} y={130} width={56} height={4} />
+    <rect x={136} y={130} width={56} height={4} className="opacity-60" />
     <rect x={136} y={138} width={56} height={6} />
 
-    <rect x={200} y={80} width={56} height={26} />
-    <rect x={200} y={110} width={56} height={16} />
+    <rect x={200} y={80} width={56} height={26} className="opacity-60" />
+    <rect x={200} y={110} width={56} height={16} className="opacity-80" />
     <rect x={200} y={130} width={56} height={14} />
   </svg>
 );
